6fbbb3b4dde7eba0012b8ae649088b2e3d2401d3
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-11-09 16:59:05 -0600

    update python script

--- sparsify_models.py
+++ sparsify_models.py
@@ -1,5 +1,9 @@
 from torch.nn.utils import prune
 import copy
+from transformers import RobertaModel, GPT2Model, BartModel
+from transformers import GPT2ForSequenceClassification, GPT2Tokenizer
+
+SPARSITY_LIST = [.1, .5, .9, .95, .99]
 
 def get_weight_parameters(layer):
     '''
@@ -36,3 +40,40 @@ def prune_weight_parameters(model, prune_amount):
         except Exception as e:
             print(e)
     return model
+
+
+
+def save_pruned_models():
+    roberta_model = RobertaModel.from_pretrained("roberta-large")
+    gpt2_model = GPT2Model.from_pretrained("gpt2")
+    bart_model = BartModel.from_pretrained("facebook/bart-large")
+
+    model_list = [("roberta", roberta_model), ("gpt2", gpt2_model), ("bart", bart_model)]
+
+    for name, model in model_list:
+        for sparsity in SPARSITY_LIST:
+            model_to_prune = copy.deepcopy(model)
+            pruned_model = prune_weight_parameters(model_to_prune, sparsity)
+            pruned_model.save_pretrained(f"models/{name}_{sparsity}")
+
+
+def save_pruned_models_glue():
+    # Define a padding token and save
+    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.save_pretrained(f"models/gpt2-glue-tokenizer")
+
+    # Add padding to gpt2 in order to do GLUE task
+    gpt2_glue = GPT2ForSequenceClassification.from_pretrained("gpt2", num_labels=3)
+    gpt2_glue.config.pad_token_id = tokenizer.pad_token_id
+    # Save gpt2 model with padding
+    gpt2_glue.save_pretrained("models/gpt2-glue")
+
+    for sparsity in SPARSITY_LIST:
+        model_to_prune = copy.deepcopy(gpt2_glue)
+        pruned_model = prune_weight_parameters(model_to_prune, sparsity)
+        pruned_model.save_pretrained(f"models/gpt2-glue_{sparsity}")
+
+if __name__ == "__main__":
+    save_pruned_models()
+    save_pruned_models_glue()
