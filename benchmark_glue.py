f6ec6969165a793caab067f812e41482286a0abe
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-11-10 13:28:22 -0600

    update benchmark code

--- benchmark_glue.py
+++ benchmark_glue.py
@@ -67,9 +67,53 @@ def run_benchmark(model_name):
         save2Json(trains.metrics, filename=f'output/glue-{model_name}_{SPARSE_PERCENT[idx]}.json')
 
 
+def run_benchmark_single(model_name, tokenizer_checkpoint, model_checkpoint, SPARSE_PERCENT):
+    raw_datasets = datasets.load_dataset('glue', 'mrpc')
+    
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_checkpoint)
+    def tokenize_function(sample):
+        return tokenizer(sample['sentence1'], sample['sentence2'], truncation=True)
+    tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+    model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint, num_labels=2)
+
+    training_args = TrainingArguments(
+        num_train_epochs = 1,
+        output_dir=f'output/glue-{model_name}_{SPARSE_PERCENT}/',
+    )
+
+    trainer = Trainer(
+        model,
+        training_args,
+        train_dataset=tokenized_datasets["train"],
+        eval_dataset=tokenized_datasets["validation"],
+        data_collator=data_collator, 
+        tokenizer=tokenizer,
+        # compute_metrics=compute_metrics
+    )
+    print(f"Start training {model_checkpoint}...")
+
+    trains = trainer.train()
+    # predictions = trainer.predict(tokenized_datasets['validation'])
+    save2Json(trains.metrics, filename=f'output/glue-{model_name}_{SPARSE_PERCENT}.json')
+
+
 if __name__ == "__main__":
     model_name = sys.argv[1].strip()
-    run_benchmark(model_name)
+    tokenizer_checkpoint = sys.argv[2].strip()
+    model_checkpoint = sys.argv[3].strip()
+    SPARSE_PERCENT = sys.argv[4].strip()
+
+    run_benchmark_single(
+        model_name, 
+        tokenizer_checkpoint, 
+        model_checkpoint, 
+        SPARSE_PERCENT
+    )
+
+
+    # run_benchmark(model_name)
+
 
 
 
--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ python benchmark_glue.py gpt2
 python benchmark_glue.py bart
 
 python benchmark_glue.py roberta
+
+python benchmark_glue.py gpt2 ./models/gpt2-glue-tokenizer/ ./models/gpt2-glue_0.1/ 10
