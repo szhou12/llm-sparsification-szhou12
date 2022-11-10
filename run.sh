107b95b4ac17525f8f47c1c6628d227e816c8008
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-11-09 18:04:37 -0600

    update changes

--- run.sh
+++ run.sh
@@ -23,3 +23,7 @@ torchrun pytorch/language-modeling/run_clm.py \
     --do_eval \
     --num_train_epochs 1 \
     --output_dir output
+
+torchrun pytorch/language-modeling/run_clm.py --model_name_or_path "$(pwd)"/models/gpt2_0.1 --tokenizer_name gpt2 --dataset_name wikitext --dataset_config_name wikitext-2-raw-v1 --per_device_train_batch_size 8 --per_device_eval_batch_size 8 --do_train --do_eval --num_train_epochs 1 --output_dir output
+
+torchrun pytorch/language-modeling/run_clm.py --model_name_or_path gpt2 --tokenizer_name gpt2 --dataset_name wikitext --dataset_config_name wikitext-2-raw-v1 --per_device_train_batch_size 8 --per_device_eval_batch_size 8 --do_train --do_eval --num_train_epochs 1 --output_dir output
