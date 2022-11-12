6712fac323a311f4ee8e4675f69f0ad5a589af59
Shuyu (Joshua) Zhou <35708194+szhou12@users.noreply.github.com>
2022-11-11 18:59:27 -0600

    upload loss runtime plots

--- run.sh
+++ run.sh
@@ -9,7 +9,6 @@ python benchmark_clm.py bart
 python benchmark_clm.py roberta
 
 
-
 # python benchmark_glue.py gpt2 ./models/gpt2-glue-tokenizer/ ./models/gpt2-glue_0.1/ 10
 # python benchmark_glue.py gpt2 ./models/gpt2-glue-tokenizer/ ./models/gpt2-glue_0.5/ 50
 # python benchmark_glue.py gpt2 ./models/gpt2-glue-tokenizer/ ./models/gpt2-glue_0.9/ 90
@@ -17,7 +16,26 @@ python benchmark_clm.py roberta
 # python benchmark_glue.py gpt2 ./models/gpt2-glue-tokenizer/ ./models/gpt2-glue_0.99/ 99
 # python benchmark_glue.py gpt2 ./models/gpt2-glue-tokenizer/ ./models/gpt2-glue/ 0
 
-python benchmark_glue.py gpt2 gpt2 ./models/gpt2_0.1/ 10
+# python benchmark_clm.py gpt2 gpt2 gpt2 0
+# python benchmark_clm.py gpt2 gpt2 ./models/gpt2_0.1/ 10
+# python benchmark_clm.py gpt2 gpt2 ./models/gpt2_0.5/ 50
+# python benchmark_clm.py gpt2 gpt2 ./models/gpt2_0.9/ 90
+# python benchmark_clm.py gpt2 gpt2 ./models/gpt2_0.95/ 95
+# python benchmark_clm.py gpt2 gpt2 ./models/gpt2_0.99/ 99
+
+# python benchmark_clm.py bart facebook/bart-large facebook/bart-large 0
+# python benchmark_clm.py bart facebook/bart-large ./models/bart_0.1/ 10
+# python benchmark_clm.py bart facebook/bart-large ./models/bart_0.5/ 50
+# python benchmark_clm.py bart facebook/bart-large ./models/bart_0.9/ 90
+# python benchmark_clm.py bart facebook/bart-large ./models/bart_0.95/ 95
+# python benchmark_clm.py bart facebook/bart-large ./models/bart_0.99/ 99
+
+
+# python benchmark_clm.py roberta roberta-large ./models/roberta_0.1/ 10
+# python benchmark_clm.py roberta roberta-large ./models/roberta_0.5/ 50
+# python benchmark_clm.py roberta roberta-large ./models/roberta_0.9/ 90
+# python benchmark_clm.py roberta roberta-large ./models/roberta_0.95/ 95
+# python benchmark_clm.py roberta roberta-large ./models/roberta_0.99/ 99
 
 
 
